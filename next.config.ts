95017d774d6f2abc45a42ba1adead577e315e822
dmingke <73776136+dmingke@users.noreply.github.com>
2025-01-21 01:46:28 +0800

    remove

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
-  env: {
-    STATIC_URL: isProd ? process.env.STATIC_URL : "",
-  },
-  assetPrefix: isProd ? process.env.STATIC_URL : "",
 };
 
 module.exports = nextConfig;
